b5218875d0de0b0c363c4698199327e2cf2950bc
Roopa-Sri3 <roopasri.gaddam@gmail.com>
2024-07-25 11:49:20 +0530

    looking for mainfile

---
+++ vars/mainjenkinsfile.groovy
@@ -0,0 +1,55 @@
+@Library('sharedlibrary') _
+pipeline{
+    agent any
+    stages{
+        stage('Build-main'){
+            when{
+                branch "main"
+            }
+            steps {
+                buildProject()
+            }
+        }
+        stage('Deploy-main'){
+            when{
+                branch "main"
+            }
+            steps {
+               deployProject('/var/www/')  
+            }
+        }
+        stage('Build-dev'){
+            when{
+                branch "dev"
+            }
+            steps {
+                buildProject()
+            }
+        }
+        stage('Deploy-dev'){
+            when{
+                branch "dev"
+            }
+            steps {
+                deployProject('/var/www/')
+            }
+        }
+        stage('Build-feature'){
+            when{
+                branch "feature"
+            }
+            steps {
+                buildProject()
+            }
+        }
+        stage('Deploy-feature'){
+            when{
+                branch "feature"
+            }
+            steps {
+                deployProject('/var/www/')
+            }
+        }
+        
+    }
+}
